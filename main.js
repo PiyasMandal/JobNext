bf2ec4cf8775dc79a6bb5b32381a3e8c40ae5324
Piyas <piyasmandal306@gmail.com>
2025-08-16 00:40:19 +0530

    meow

---
+++ main.js
@@ -0,0 +1,48 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+ menuBtn.addEventListener("click", (e) => 
+{
+navLinks.classList.toggle("open");  
+const isOpen = navLinks.classList.contains("open");  
+menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line"); }); 
+
+navLinks.addEventListener("click", (e) => {  
+ navLinks.classList.remove("open");   
+ menuBtnIcon.setAttribute("class", "ri-menu-line"); 
+});  
+const scrollRevealOption = {  
+distance: "50px",
+origin: "bottom",   
+duration: 1000,
+ };  
+ScrollReveal().reveal(".headercontainer h2",
+{   ...scrollRevealOption, 
+
+}); 
+ScrollReveal().reveal(".headercontainer h1", {
+...scrollRevealOption,   
+delay: 500, 
+});
+ScrollReveal().reveal(".headercontainer p", 
+{ ...scrollRevealOption,   
+delay: 1000, });
+ScrollReveal().reveal(".headerbtns", { 
+...scrollRevealOption,  
+delay: 1500, 
+});  
+ScrollReveal().reveal(".stepscard", { 
+...scrollRevealOption, interval: 500,
+ });  
+ScrollReveal().reveal(".explorecard", {  
+ duration: 1000,   interval: 500,
+ });  
+ScrollReveal().reveal(".jobcard", 
+{...scrollRevealOption,   interval: 500, }); 
+ScrollReveal().reveal(".offercard", 
+{...scrollRevealOption,   interval: 500, }); 
+
+const swiper = new Swiper(".swiper", {  
+loop: true, 
+});
